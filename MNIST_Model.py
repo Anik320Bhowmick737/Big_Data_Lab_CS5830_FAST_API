7d51abdad7bfc76e0dc969cdaf194d4dc2b24de4
Anik <anik.go.fast@gmail.com>
2024-04-25 16:27:41 +0530

    Added comments for MNIST_Model.py

--- MNIST_Model.py
+++ MNIST_Model.py
@@ -6,8 +6,9 @@ from keras.models import Sequential
 from keras.optimizers import Adam 
 from keras.losses import SparseCategoricalCrossentropy
 import numpy as np
-
+# for training the model with the best hyperparameters of previous assignment 
 def get_architecture():
+    # sequentail definition of the model
     model = Sequential(
 [
     Input(shape=(784,)),
@@ -21,18 +22,19 @@ def train_model():
     model = get_architecture()
     model.compile(loss=SparseCategoricalCrossentropy(),metrics=['acc'])
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    
-    #x_train = RandomRotation(0.2,fill_mode='nearest',interpolation='nearest')(x_train)
-    #x_test = RandomRotation(0.2,fill_mode='nearest',interpolation='nearest')(x_test)
+    # make sure the images are in white backgrround instead of black background it will make the inference easier for hand written digits for testing
     x_train=255-x_train
     x_test=255-x_test
+    # flatten the datapoints to 784 dimensional vector as the model is simple ANN and expects linear data points
     x_train = np.array(x_train).reshape(60000,-1)/255.0
     x_test = np.array(x_test).reshape(10000,-1)/255.0
 
     model.fit(x_train,y_train, validation_split = 0.2,epochs =10)
     model.evaluate(x_test,y_test)
+    # save the model for use in FAST API later
     model.save('MNIST_model.keras')
 
 if __name__=="__main__":
     train_model()
+    # start the code from here
 
